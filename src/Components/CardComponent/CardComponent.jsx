5a2658258515d785724f0236d08521fc22344e39
umafuster <umafuster@gmail.com>
2023-10-13 13:07:06 -0300

    Agrego Productos en Tarjeta

---
+++ src/Components/CardComponent/CardComponent.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+const CardComponent = ({item}) => {
+const {img, name, description, date} = item;
+    return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={img}
+          alt="Producto"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {date}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default CardComponent;
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+// CSS
 import "./ItemListContainer.css";
 
-export const ItemListContainer = ({greeting}) => {
+//Componentes
+import CardComponent from '../CardComponent/CardComponent';
+import {Link} from 'react-router-dom';
+
+const ItemListContainer = ({greeting}) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios('/products.json').then((res) =>
+    setItems(res.data)
+    );
+  }, []);
   return (
     <div>
         <h1 className='bienvenida'>{greeting}</h1>
+    
+    {items.map((item) => {
+      return (
+        <div key={item.id}>
+        <Link to={`/detail/${item.id}`}>
+          <CardComponent item={item} />
+        </Link>
+        </div>
+      );
+    })}
     </div>
   );
 };
