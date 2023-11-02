35e5e2a12beda0044a725a60741799cf51e4bc9e
umafuster <umafuster@gmail.com>
2023-11-02 09:27:03 -0300

    Modifico estilos

--- src/Components/CardComponent/CardComponent.jsx
+++ src/Components/CardComponent/CardComponent.jsx
@@ -7,29 +7,30 @@ import Typography from '@mui/material/Typography';
 //CSS
 import "./CardComponent.css"
 
-const CardComponent = ({item}) => {
-const {name, date, description, img} = item;
-    return (
+const CardComponent = ({ item }) => {
+  const { name, date, description, img } = item;
+  return (
     <Card sx={{ maxWidth: 350 }}>
-      
-        <CardMedia
-          component="img"
-          height="200"
-          image={img}
-          alt="Producto"
-        />
-        <CardContent className='card-typo'>
-          <Typography className='card-typo' gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography className='card-typo' variant="subtitle1" color="text.secondary">
-            {date}
-          </Typography>
-          <Typography className='card-typo' variant="body3" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      
+
+      <CardMedia
+        className='card-container'
+        component="img"
+        height="500"
+        image={img}
+        alt="Producto"
+      />
+      <CardContent className='card-typo'>
+        <Typography className='card-typo' gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography className='card-typo' variant="subtitle1" color="text.secondary">
+          {date}
+        </Typography>
+        <Typography className='card-typo' variant="body3" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+
     </Card>
   );
 };
--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 //Component
 import ItemListContainer from '../../Components/ItemListContainer/ItemListContainer';
 
+//CSS
+import "./ProductPage.css"
+
 const ProductPage = () => {
     return (
         <div>
-            <h1>ProductPage</h1>
+            <h1 className='productpage-title'>Products</h1>
 
             <ItemListContainer />
         </div>
